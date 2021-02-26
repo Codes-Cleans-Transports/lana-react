59325c864ba2af07b9fc68b19c34173a4a33e858
Ivan Stefanov <isteffanov@gmail.com>
2021-02-27 01:03:30 +0200

    signup page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SignInPage from './pages/SignInPage';
 
 function App() {
   return (
     <div className="App">
-      yay
+      kur
+      <SignInPage/>
     </div>
   );
 }
